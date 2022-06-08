39b107b9977697696c60bf42cd0d5e0f3b9686f7
Andys Kurniawan <ansanwan@gmail.com>
2022-06-08 08:16:21 +0700

    change node modules path

--- application/views/template/backend/layout.blade.php
+++ application/views/template/backend/layout.blade.php
@@ -173,11 +173,11 @@
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
     <script src="https://getbootstrap.com/docs/5.2/examples/dashboard/dashboard.js"></script>
-    <script type="text/javascript" src="{{ base_url('assets/node_modules') }}/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="{{ base_url('node_modules') }}/jquery/dist/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.js"></script>
 
-    <script src="{{ base_url('assets/node_modules') }}/jquery-datetimepicker/build/jquery.datetimepicker.full.min.js"></script>
-    <script src="{{ base_url('assets/node_modules') }}/print-js/dist/print.js"></script>
+    <script src="{{ base_url('node_modules') }}/jquery-datetimepicker/build/jquery.datetimepicker.full.min.js"></script>
+    <script src="{{ base_url('node_modules') }}/print-js/dist/print.js"></script>
 
 
     <script type="text/javascript">
