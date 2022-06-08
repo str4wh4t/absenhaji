1bd7f9924a17fc2f34373fd8b2ced0bd2aea635c
Andys Kurniawan <ansanwan@gmail.com>
2022-06-08 08:10:55 +0700

    node_modules

--- symlink.php
+++ symlink.php
@@ -1,6 +1,6 @@
 <?php
     $target = __DIR__ . DIRECTORY_SEPARATOR . 'writable/node_modules';
-    $link = __DIR__ . DIRECTORY_SEPARATOR . 'public/assets/node_modules';
+    $link = __DIR__ . DIRECTORY_SEPARATOR . 'public/node_modules';
     if (symlink( $target, $link )) {
         echo "SYMLINK - CREATED \n";
     } else {
