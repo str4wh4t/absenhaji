7a20bc4a75182aba3145b404d66618337a150f24
Andys Kurniawan <ansanwan@gmail.com>
2022-06-10 14:04:24 +0700

    jabatan struktural

--- application/views/backend/User/form.blade.php
+++ application/views/backend/User/form.blade.php
@@ -33,16 +33,16 @@
     <input type="text" class="form-control" id="username" name="username" value="{{ $input['username'] }}">
   </div>
   <div class="mb-3">
-    <label for="jabatan_struktural" class="form-label">Jabatan Struktural</label>
-    <?= form_dropdown('struktural', @$jabatan_struktural, @$input['struktural_id'] , 'class="form-select chosen" data-placeholder="- PILIH -"') ?>
-  </div>
-  <div class="mb-3">
     <label for="bidang" class="form-label">Bidang</label>
     <?= form_dropdown('bidang', $bidang, $input['bidang_id'] , 'class="form-select chosen" data-placeholder="- PILIH -"') ?>
   </div>
   <div class="mb-3">
     <label for="instansi" class="form-label">Instansi</label>
     <?= form_dropdown('instansi', $instansi, $input['instansi_id'] , 'class="form-select chosen" data-placeholder="- PILIH -"') ?>
+  </div>
+  <div class="mb-3">
+    <label for="jabatan_struktural" class="form-label">Jabatan Struktural</label>
+    <?= form_dropdown('struktural', @$jabatan_struktural, @$input['struktural_id'] , 'class="form-select chosen" data-placeholder="- PILIH -"') ?>
   </div>
   <div class="mb-3">
     <label for="jabatan" class="form-label">Jabatan</label>
