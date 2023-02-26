4051fd88dbd96a7902a0d49f10ca070d869c8178
Djunet <4199003+Djunet@users.noreply.github.com>
2023-02-26 15:23:13 +0700

    Update login.blade.php

--- application/views/Auth/login.blade.php
+++ application/views/Auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <form method="post" action="{{ site_url('login') }}">
     <!-- <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
-    <h1 class="h3 mb-3 fw-normal">SATGAS HAJI</h1>
+    <h1 class="h3 mb-3 fw-normal">PETUGAS HAJI</h1>
 
     @if($login_salah != '')
     <div class="alert alert-danger" role="alert">
